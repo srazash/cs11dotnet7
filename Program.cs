5a0beb2271b5975ebb61edc241def9780dab1225
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-04-03 17:24:53 +0100

    Continuing work on ch4

--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
 ﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
-RunFibImperative();
-RunFibFunctional();
+//RunFibImperative();
+//RunFibFunctional();
+
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+Console.WriteLine($"Writing to: {logPath}");
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+Trace.Listeners.Add(logFile);
+Trace.AutoFlush = true;
 
 Debug.WriteLine("Debug says, I am watching!");
-Trace.WriteLine("Trace syas, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+WriteLine("Reading config from appsettings.json in {0}.",
+    arg0: Directory.GetCurrentDirectory());
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+    displayName: "Trace",
+    description: "This switch is set via JSON config.");
+
+configuration.GetSection("Trace").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
+Console.ReadLine();
